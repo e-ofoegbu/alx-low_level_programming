d8a40b4dbcad0314b8d9c96f2ae9d7e05ba5740d
Enyinnaya <enyinnayaofoegbu@gmail.com>
2023-07-27 17:51:01 -0700

    assembly language curve ball

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,24 @@
+; File: 101-hello_holberton.asm
+; Author: Enyinnaya Ofoegbu
+
+extern printf
+
+section .text
+   global main
+
+main:
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
